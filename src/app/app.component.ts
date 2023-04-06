60f30790639723692a35e200293f87e036141e34
monalishapoddar <monalisha027@gmail.com>
2023-04-06 15:36:28 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  x=5;
+  y=4;
+  difference:any;
+  sum:any;
+  subtraction(){
+    this.difference= (this.x - this.y)
+  }
+  addition(){
+    this.sum=(this.x+ this.y)
+  }
+
+  Increment(){
+    this.sum++;
+
+  }
+  Decrement(){
+    this.difference--;
+
+  }
+  Reset(){
+    this.x=0;
+    this.y=0;
+    this.sum=0;
+    this.difference=0;
+  }
+  configuration: any="";
+  constructor(){
+    console.log(environment.environment_title);
+    this.configuration = environment;
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    environment_title: 'Dev',
+ 
+};
